66830db893e6311274fbb0d7962f6e54393c5f46
Muhammad Firdaus <vicevirus@Silo.local>
2023-03-30 12:25:34 +0800

    Trying out PWA

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    modules: [
+        '@kevinmarrec/nuxt-pwa'
+    ],
     ssr: true,
     css: ['vuetify/lib/styles/main.sass'],
     build: {
-      transpile: ['vuetify'],
+        transpile: ['vuetify'],
     },
     vite: {
-      define: {
-        'process.env.DEBUG': false,
-      },
+        define: {
+            'process.env.DEBUG': false,
+        },
     },
-  })
+})
