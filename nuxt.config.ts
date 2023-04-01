3bd1725ba1e402cf10188cdca584637a509527d8
Muhammad Firdaus <vicevirus@Silo.local>
2023-04-02 07:27:01 +0800

    eureka

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
 export default defineNuxtConfig({
-    
+    plugins: [{ src: '~/plugins/vercel.js', mode: 'client' }],
   
     
     modules: [
